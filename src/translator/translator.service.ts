d635262aab0492aa3709b6fdb663725f734e5953
vip <vip@DESKTOP-TVLU5IH>
2022-05-05 19:16:04 +0200

    change in cors

--- src/translator/translator.service.ts
+++ src/translator/translator.service.ts
@@ -4,7 +4,8 @@ const { translate } = require('free-translate');
 @Injectable()
 export class TranslatorService {
     async translate({from,to,query}:TranslateDTO):Promise<string>{
-        const response=await translate(query, {from,to});
+      //  const response=await translate(query, {from,to});
+       const response='here will retreive the translation'
         return response
         
      }
