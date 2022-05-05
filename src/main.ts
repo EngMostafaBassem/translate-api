abb9afda02de179a8fd09024393b1d569c81a825
vip <vip@DESKTOP-TVLU5IH>
2022-05-05 19:27:02 +0200

    increase timeout for a request

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+var timeout = require('connect-timeout')
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+   const app = await NestFactory.create(AppModule);
    app.enableCors()
-  await app.listen(process.env.PORT||3000);
+   app.use(timeout('50s'))
+   await app.listen(process.env.PORT||3000);
 }
 bootstrap();
