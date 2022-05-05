0c304fff21b9ef2247ec9456a33b09c72e192ea0
vip <vip@DESKTOP-TVLU5IH>
2022-05-05 18:38:18 +0200

    finalize setup env for heroku

--- src/translator/translator.service.ts
+++ src/translator/translator.service.ts
@@ -4,6 +4,7 @@ const { translate } = require('free-translate');
 @Injectable()
 export class TranslatorService {
     async translate({from,to,query}:TranslateDTO):Promise<string>{
-        return translate(query, {from,to});
+        //return translate(query, {from,to});
+        return "test"
      }
 }
