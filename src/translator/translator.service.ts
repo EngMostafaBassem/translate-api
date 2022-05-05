dde200be294a78e9008546940c481c8f25c787a2
vip <vip@DESKTOP-TVLU5IH>
2022-05-05 19:20:24 +0200

    change in pacakge.json

--- src/translator/translator.service.ts
+++ src/translator/translator.service.ts
@@ -4,8 +4,7 @@ const { translate } = require('free-translate');
 @Injectable()
 export class TranslatorService {
     async translate({from,to,query}:TranslateDTO):Promise<string>{
-      //  const response=await translate(query, {from,to});
-       const response='here will retreive the translation'
+       const response=await translate(query, {from,to});    
         return response
         
      }
